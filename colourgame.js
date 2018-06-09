b29a44de474c8e049e726dc6b7fb9b71185e1afc
leo lightfoot <iam.abdulmalik7@gmail.com>
2018-06-09 20:22:42 +0530

    adding js files

---
+++ colourgame.js
@@ -0,0 +1,10 @@
+var NumSquares = 6;
+var colours = [];
+var pickedcolour;
+var squares = document.querySelectorAll(".square");
+var colourdisplay = document.querySelector("#colourdisplay");
+var messagedisplay = document.querySelector("#mesaage");
+var h1 = document.querySelector("h1");
+var resetbutton = document.querySelector("#reset");
+var mode = document.querySelector(".mode");
+
